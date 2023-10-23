256eceb1e9c2b459881fc19ea284bbf569dee5db
RebeccaElusme <elusmerebecca1@gmail.com>
2023-10-22 21:13:19 -0400

    Database test

---
+++ Backend/index.js
@@ -0,0 +1,62 @@
+//const { default: userEvent } = require('@testing-library/user-event');
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const pool = mysql.createPool({
+host: process.env.MYSQL_HOST,
+user: process.env.MYSQL_USER,
+password: process.env.MYSQL_PASSWORD,
+database: process.env.MYSQL_DATABASE
+
+}).promise();
+
+// get all the properties
+async function getProperties() 
+        {
+
+        const [rows] = await pool.query("SELECT * FROM Properties.Property")
+        console.log(rows)
+             return rows
+                 }
+
+// For search after ... 
+async function getProperty(id){
+    const [rows] = await pool.query(`
+    SELECT * 
+    FROM Properties.Property
+    WHERE idTest = ?`, [id])
+        return rows[0]
+}
+
+// to create a new property
+async function createProperty(Price, Adress, PostalCode, City, Province,
+                                 Neighborhood, Baths, Beds, 
+                                 Property_type, Listing_type )
+ {
+             
+        const [result] = await pool.query(`
+        INSERT INTO Property (price, adress, Postal_Code, City, Province, Neighborhood, No_Bathrooms, No_Bedrooms, 
+                Property_type_id, Listing_type_id )
+        VALUES (?,?,?,?,?,?,?,?,?,?)
+        `, [Price, Adress, PostalCode, City, Province, Neighborhood, Baths, Beds, 
+                Property_type, Listing_type ] )
+        
+}
+
+//async function updateProperty
+
+// to delete a property
+async function deleteProperty(id){
+        const [result]= await pool.query(`
+        DELETE FROM Property 
+        WHERE property_id =?`, [id])
+
+}
+
+//const result = await getProperties()
+//console.log(result)
+
+//getProperties()
+//const result = await createProperty(900, "Reussix", "H3H3H2", "Montreal", "Quebec",null, 4, 5,1,2)
+const result1 = await deleteProperty(64)
