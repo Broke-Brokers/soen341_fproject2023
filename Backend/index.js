77468230b8a3270a0f199d2b5b1aa9a281b350a3
isaisa002 <118073058+isaisa002@users.noreply.github.com>
2023-10-24 20:45:18 -0400

    Connected Mysql to a local host using Node js

--- Backend/index.js
+++ Backend/index.js
@@ -1,37 +1,27 @@
-//const { default: userEvent } = require('@testing-library/user-event');
-import mysql from 'mysql2'
-import dotenv from 'dotenv'
-dotenv.config()
+import express from "express"
+import mysql from "mysql"
+const app = express()
+const db=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"=1234Lovenature",
+    database:"test"
+})
 
-const pool = mysql.createPool({
-host: process.env.MYSQL_HOST,
-user: process.env.MYSQL_USER,
-password: process.env.MYSQL_PASSWORD,
-database: process.env.MYSQL_DATABASE
+app.get("/", (req,res)=>{
+    res.json("Hello backend")
+})
 
-}).promise();
+app.get("/buyer", (req, res)=>{
+    const q= "SELECT * FROM buyer"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+    
+})
 
-async function getTests() {
-//pool.query("INSERT INTO Test (idTest, adress) VALUES (2,'test!');")
-const [rows] = await pool.query("SELECT * FROM Properties.Test")
-        return rows
-}
 
-async function getTest(id){
-    const [rows] = await pool.query(`
-    SELECT * 
-    FROM Properties.Test
-    WHERE idTest = ?`, [id])
-        return rows[0]
-}
-
-async function createTest(idTest, adress){
-        const [result] = await pool.query(`
-        INSERT INTO Test (idTest, adress)
-        VALUES (?,?)
-        `, [idTest,adress] )
-        
-}
-
-const result = await createTest(4,'Test')
-console.log(result)
+app.listen(8800, ()=>{
+    console.log("fonctionne!")
+})
