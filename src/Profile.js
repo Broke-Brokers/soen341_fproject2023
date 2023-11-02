37108c1162ff074e6dcfa3c1c123b50efde17839
RebeccaElusme <elusmerebecca1@gmail.com>
2023-11-02 15:01:58 -0400

    added isabelle broker profile on website

--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react'
 //import Add_Button from './components/buttons/Add_Button'
 import Creation_Property from './components/Profile-page/Creation_Property'
+import Admin_Brokers_Grid from './components/Profile-page/Admin_Brokers_Grid'
 //import Profile from '../Profile'
 
 
@@ -10,6 +11,8 @@ function Profile() {
     <div>
     
       <Creation_Property/>
+      <Admin_Brokers_Grid/>
+
     </div>
 
   )
