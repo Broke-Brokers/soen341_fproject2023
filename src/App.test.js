c15d5480b86ed3330e9084cc803e24a47b938ef0
DominiqueProulx <dominique.proulx@gmail.com>
2023-11-02 22:45:22 -0400

    added testing of rendering without crashing home in app.test and created a unit test in the branch

--- src/App.test.js
+++ src/App.test.js
@@ -6,5 +6,9 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Home />, div);
+});
 
 {/*Basic smoke test for component taken from :https://create-react-app.dev/docs/running-tests  */}
