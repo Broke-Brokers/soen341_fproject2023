ae1fc442886c99dcab81c9807d9529ff25fd27be
JPEUMZZ <jounjuniorpaul@gmail.com>
2023-11-05 00:14:18 -0400

    search for broker and for properties

--- src/Pages/Broker.js
+++ src/Pages/Broker.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Request_visite from '../components/Request_visite'
+import Search_Result_Grid from '../components/search-for-brokers/Search_Result_Grid'
+
 function Broker() {
   return (
     <div>
-      
+      <Search_Result_Grid/>
       <Request_visite/>
     </div>
   )
