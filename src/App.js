0634340ea3b1795c23bdf357a649e5ad08e9d835
DominiqueProulx <dominique.proulx@gmail.com>
2023-11-05 19:31:16 -0500

    changed the cookie valye to 0 for now

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ import Cookies from 'js-cookie';
 
 // set the cookie for usertype
 //guest = 0 , system admin =1 , broker=2 , homebuyer/renter =3
-Cookies.set('usertypeID', '2')
+Cookies.set('usertypeID', '0')
 
 function App() {
 const usertype = Cookies.get('usertypeID');
