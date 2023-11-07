d06816772fb11d66c29852a4e62f840afd0b27c4
isaisa002 <118073058+isaisa002@users.noreply.github.com>
2023-11-07 03:11:56 -0500

    resolve conflict because profile.js moved in pages folder

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -6,7 +6,7 @@ import Admin_Brokers_Grid from '../components/Profile-page/Admin_Brokers_Grid';
 //import Crud_broker_result_gallery from './components/Profile-page/CrudBrokers/Crud_broker_result_gallery'
 
 //import Profile from '../Profile'
-import Card_home from './components/search-page/Card_home';
+import Card_home from '../components/search-page/Card_home';
 
 
 
