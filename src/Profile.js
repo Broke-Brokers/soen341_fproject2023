10ee519151a79721a394e5f2b82ac9f14a2d8f41
isaisa002 <118073058+isaisa002@users.noreply.github.com>
2023-11-06 19:14:24 -0500

    tried broker card on profile page for crud

--- src/Profile.js
+++ src/Profile.js
@@ -1,7 +1,9 @@
 import React from 'react'
 //import Add_Button from './components/buttons/Add_Button'
 import Creation_Property from './components/Profile-page/Creation_Property'
-import Admin_Brokers_Grid from './components/Profile-page/Admin_Brokers_Grid'
+import Admin_Brokers_Grid from './components/Profile-page/Admin_Brokers_Grid';
+import Crud_broker_result_gallery from './components/Profile-page/CrudBrokers/Crud_broker_result_gallery'
+
 //import Profile from '../Profile'
 
 
@@ -12,6 +14,8 @@ function Profile() {
     
       <Creation_Property/>
       <Admin_Brokers_Grid/>
+      <Crud_broker_result_gallery/>
+      
 
     </div>
 
---
+++ src/components/Profile-page/CrudBrokers/Crud_broker_result_gallery.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import BrokerCard_forCrud from './BrokerCard_forCrud';
+
+function Crud_broker_result_gallery(){
+  /*let brokerName, email, phoneNumber; // Declare these variables in the parent scope
+
+  if (!brokerList) { // evaluates to true if brokerList is null
+     //Placeholder name to handle the case where brokerList is empty
+     brokerName = "Albus Dumbledore";
+     email = "itsleviosa@hogwarts.com";
+     phoneNumber = "514-252-2525";
+  }
+  else{
+  
+    brokerName = brokerList.FirstName;
+    email = brokerList.Email;
+    phoneNumber = brokerList.PhoneNumber;
+
+   }
+   /*The fields:
+     Broker_id integer PK
+  First_Name VARCHAR
+  Last_Name VARCHAR
+  Phone_Number VARCHAR
+  Email Varchar
+   */
+    
+
+/*
+    const profilePicture = "https://pointrussell.opencities.com/files/content/public/v/5/council/elected-members/albus-dumbledore/albus-dumbledore.jpg?dimension=pageimage&w=480";
+
+
+    return(
+        <>
+        <div className="brokerCard">
+       <div className="cardTitle">Property Broker</div>
+  <div className="profilePic">
+  
+    <img src={profilePicture} alt={brokerName} />
+  </div>
+  <div className="brokerName">
+    <h2>{brokerName}</h2>
+  </div>
+  <div className="brokerInfo">
+   <p>{email}</p>
+   <p>{phoneNumber}</p>
+  </div>
+  <div className="social">
+    <ul>
+      <li><i class="fab fa-facebook"></i></li>
+      <li><i class="fas fa-envelope"></i></li>
+            <li><i class="fas fa-phone"></i></li>
+      <li><i class="fas fa-link"></i></li>
+    </ul>
+  </div>
+</div>
+</>
+        
+        );
+        */
+        }
+export default Crud_broker_result_gallery
