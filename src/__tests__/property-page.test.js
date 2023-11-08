5b84c8090d170d83477c77d00dcc7f1cf47cf966
DominiqueProulx <dominique.proulx@gmail.com>
2023-11-07 20:42:11 -0500

    added enzyme for react components

--- src/__tests__/property-page.test.js
+++ src/__tests__/property-page.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-
 import BrokerCard from '../components/property-page/BrokerCard';
 import PropertyGallery from '../components/property-page/PropertyGallery';
-import PropertyInfo from '../components/property-page/PropertyInfo';
 
 
 it('performs snapshot testing', () => {
