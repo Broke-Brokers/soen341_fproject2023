4072921d41f9eb3859438107f151ad249c5507c7
RebeccaElusme <elusmerebecca1@gmail.com>
2023-11-08 23:34:17 -0500

    Offer_table

--- src/Pages/Offer.js
+++ src/Pages/Offer.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Offers_table from "../components/Offers/Offers_table";
 
 function Offer(){
    
     return (
         <div> 
+    
+    <Offers_table/>
     <h1>Offer sent</h1>
     <h1>Offer received</h1>
     </div>
+   
     )
 
 }
---
+++ src/components/Offers/Offers_table.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from '../../firebase_configuration.js';
+import './Offers_table.css'
+
+const Offers_table = () => {
+    const [clientOffers, setClientOffers] = useState([]); // Renamed and fixed variable name
+
+    // Set up the collection reference
+    const clientOffersCollectionRef = collection(db, "Offers");
+
+    useEffect(() => {
+        const getClientOffer = async () => {
+            const data = await getDocs(clientOffersCollectionRef);
+            const offersData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+            setClientOffers(offersData);
+        };
+
+        getClientOffer();
+    }, [clientOffersCollectionRef]); // Added dependency to prevent unnecessary re-renders
+
+    return (
+        <div className="app-container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Client Name</th>
+                        <th>Offer Price ($)</th>
+                        <th>Offer Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {clientOffers.map((offer) => (
+                        <tr key={offer.id}>
+                            <td>{offer.ClientID}</td> {/* Assuming 'ClientID' is a field in your Firestore document */}
+                            <td>{offer.Price}</td> {/* Assuming 'Price' is a field in your Firestore document */}
+                            <td>Pending </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Offers_table;
