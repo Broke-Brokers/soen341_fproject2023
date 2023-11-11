cfed92c937129858e45369cfd0aa4f6b24216376
isaisa002 <118073058+isaisa002@users.noreply.github.com>
2023-11-10 20:11:04 -0500

    snapshot for offer form completed

--- src/components/forms/offer_form.jsx
+++ src/components/forms/offer_form.jsx
@@ -3,21 +3,25 @@ import {useState, useEffect} from "react";
 import {db} from '../../firebase_configuration.js'
 import { collection, doc, getDocs, addDoc, updateDoc, onSnapshot} from "firebase/firestore";
 import './OfferForm.css'
+//import { option } from 'yargs';
 function OfferForm() {
   let brokerid = 1; 
-
   
   const [Clients, setClients] = useState([]);
   const [Offers, setOffers] = useState([]);
+  const [BuyerRenter_Client, setBuyerRenter_Client] = useState([]);
+
   const ClientCollectionRef = collection(db, "Client")
   const OffersCollectionRef = collection(db, "Offers")
+  const BuyerRenterCollectionRef = collection(db, "Buyer_Renter")
+
   const [records, setRecords] = useState([])
   
 
   const[getBrokerID_receiver, setBrokerID_receiver]= useState(0)
   const[getBrokerID_transmitter, setBrokerID_transmitter]= useState(0)
   const[getClientID, setClientID]= useState(0)
-  //const[getOfferID, setOfferID]= useState("")
+  const[getOfferID, setOfferID]= useState("")
   const[getPrice, setPrice]= useState("")
 
 
@@ -44,24 +48,48 @@ function OfferForm() {
       
   
     };
+
+    const getBuyerRenter = async () => {
+      onSnapshot(BuyerRenterCollectionRef, (snapshot)=> {
+        //snapshot.docs.maps returns all documents in our firebase
+        // doc.data return the data of each document (Field & variables associated with data)
+        setBuyerRenter_Client(snapshot.docs.map(doc=> doc.data()));
+        setRecords(snapshot.docs.map(doc=> doc.data()));
+       });
+      
+  
+    };
   
   getClients();
   getOffers();
+  getBuyerRenter();
  }, [])
 
+/*
+const FilterClient(client)=>{
+setRecords(Clients.filter((c)=>
+{if(c.BrokerID && brokerid)
+  return c.ClientName}
+))
 
 
+
+}*/
 
 
 
 
 
   const createOffers = async()=>{
+   const newOfferDocRef = doc(OffersCollectionRef);
+   setOfferID(newOfferDocRef.id);
+   
+   
     await addDoc(OffersCollectionRef,{
       BrokerID_receiver: getBrokerID_receiver,
       BrokerID_transmitter: getBrokerID_transmitter,
       ClientID: getClientID,
-      //OfferID:getOfferID,
+      OfferID:getOfferID,
       Price:getPrice
       
       
@@ -87,12 +115,19 @@ const ClientSelect=(event)=>{
      
      <div className='rightpart'>
      <select className='selection' onChange={ClientSelect} >
-        <option  value = "" > Clients </option>
-     { Clients.filter(f=> f.BrokerID== brokerid).map(option=>(
-        <option value={option.ClientID}>{ option.UserID}</option>
-
-      ))}
+        <option  value = "" > Select a client </option>
+     {Clients.filter(client =>
+     client.BrokerID === brokerid).map((option)=> (
+     
+      <option value={option.ClientID}>{option.ClientName} </option>
+      ) )}
       </select>
+
+
+
+
+
+      
       <label className='labelUserEmail'>Your email <em>&#x2a;</em></label>
       <input className='user_email' type="email" placeholder="Email"   
        onChange={(event)=> {
