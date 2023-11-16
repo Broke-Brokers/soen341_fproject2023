c715127abcd37341ac6b61720389f6bae61b2940
JPEUMZZ <jounjuniorpaul@gmail.com>
2023-11-16 15:38:08 -0500

    about Pages

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Card from '../components/Card';
-
+import About_component from '../About_component';
 function AboutUs() {
   return (
     <div>
-      
+      <About_component/>
       <Card/>
     </div>
   )
