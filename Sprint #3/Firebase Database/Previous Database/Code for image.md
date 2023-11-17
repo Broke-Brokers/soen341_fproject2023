9255299b8158df311d59b8be1010b186f199b242
DominiqueProulx <dominique.proulx@gmail.com>
2023-11-16 23:39:53 -0500

    Updates

--- Sprint #3/Firebase Database/Previous Database/Code for image.md
+++
@@ -1,4 +0,0 @@
-
-
-![Admin collection in Firebase](image.png)![Broker collection in Firebase](image-1.png)
-![Alt text](image-2.png)![Alt text](image-3.png)![Alt text](Broke_Brokers_Mockup-20-1.jpg)
--- src/__tests__/OfferForm.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-
-import offer_form from '../components/forms/offer_form';
-
-it('performs snapshot testing', () => {
-    const tree = renderer.create(<offer_form />).toJSON();
-    expect(tree).toMatchSnapshot();
-    });
